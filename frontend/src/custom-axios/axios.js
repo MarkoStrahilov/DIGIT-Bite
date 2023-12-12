b722772111aa5ceed1fac3a3f70900bd8d93c0a8
AceGjorgjievski <ace.gjorgjievski6@gmail.com>
2023-12-12 11:21:06 +0100

    added axios.js & service for fetching [digitBiteService.js]

---
+++ frontend/src/custom-axios/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const instance = axios.create({
+    // baseURL: 'http://localhost:9092/api',
+    // headers: {
+    //     'Access-Control-Allow-Origin' : '*'
+    // }
+})
+
+export default instance;
+
