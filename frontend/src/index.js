2c2e81d1799401986a13d5b9a9ff9a4bac7d51b4
AceGjorgjievski <ace.gjorgjievski6@gmail.com>
2023-12-13 13:12:13 +0100

    removed bootstrap package

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
 
