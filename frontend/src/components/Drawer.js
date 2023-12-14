da6c45041d943ed8dce250cd8840b29facbf3745
AceGjorgjievski <ace.gjorgjievski6@gmail.com>
2023-12-14 16:15:24 +0100

    added delete button for deleting items from the shopping cart

--- frontend/src/components/Drawer.js
+++ frontend/src/components/Drawer.js
@@ -1,5 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import Navbar from './Navbar'
+import {Flex} from "@chakra-ui/react";
+import {Box, Text} from '@chakra-ui/react'
+import {auth, db, storage} from '../repository/firebase/firebase';
 
 import {
     Button,
@@ -14,14 +17,13 @@ import {
     useDisclosure,
     IconButton
 } from "@chakra-ui/react";
-import { FaShoppingCart } from "react-icons/fa";
-import { GlobalStyles } from '../constants/GlobalStyles';
+import {FaShoppingCart} from "react-icons/fa";
+import {GlobalStyles} from '../constants/GlobalStyles';
 import firebase from "firebase";
-import {db} from "../repository/firebase/firebase";
 
 
 const SideDrawer = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure()
+    const {isOpen, onOpen, onClose} = useDisclosure()
     const [placement, setPlacement] = useState('right')
 
 
@@ -59,6 +61,18 @@ const SideDrawer = () => {
         });
     };
 
+    const handleDeleteProduct = (mealId) => {
+        console.log("deleting...")
+        const cartProductRef = db.collection('Cart ' + userUid).doc(mealId);
+
+        cartProductRef.delete()
+            .then(() => {
+                console.log('Product deleted successfully');
+            })
+            .catch((error) => {
+                console.error('Error deleting product:', error);
+            });
+    }
 
 
     return (
@@ -74,26 +88,35 @@ const SideDrawer = () => {
                 p={25}
                 mt={"25px"}
                 ml={10}
-                icon={<FaShoppingCart />}
+                icon={<FaShoppingCart/>}
             />
             <Drawer placement={placement} onClose={onClose} isOpen={isOpen}>
-                <DrawerOverlay />
+                <DrawerOverlay/>
                 <DrawerContent>
                     <DrawerHeader borderBottomWidth='1px'>Basic Drawer</DrawerHeader>
                     <DrawerBody>
-                        {cartProducts.map(product => {
-                            return (
-                                <ul>
-                                    <li>
-                                        {product.title}
-                                    </li>
-                                </ul>
-                            );
-                        })}
-                        {/*<p>Some contents...</p>*/}
-                        {/*<p>Some contents...</p>*/}
-                        {/*<p>Some contents...</p>*/}
+                        {cartProducts.map(product => (
+                            <Flex key={product.mealId} alignItems="center" justify="space-between">
+                                <Text flex="1" marginRight="2" fontSize="1.5em">
+                                    <span role="img" aria-label="dot" style={{ marginRight: '5px' }}>
+                                        &#183;
+                                    </span>
+                                    {product.title}
+                                </Text>
+
+
+                                <Button
+                                    width="80px"  // Adjust the width as needed
+                                    colorScheme="red"
+                                    size="sm"
+                                    onClick={() => handleDeleteProduct(product.mealId)}
+                                >
+                                    Delete
+                                </Button>
+                            </Flex>
+                        ))}
                     </DrawerBody>
+
                 </DrawerContent>
             </Drawer>
         </>
