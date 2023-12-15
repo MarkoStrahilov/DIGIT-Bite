e84234b7aca6704e5941aa82af78af97d10593e1
GorgievskiDaniel <daniel.gorgievski6@gmail.com>
2023-12-15 09:48:34 +0100

    Making about page

--- frontend/src/layout/About.js
+++ frontend/src/layout/About.js
@@ -1,10 +1,18 @@
 import React, {useEffect, useState} from 'react'
-import {Box} from '@chakra-ui/react'
+import {Box, Button, Flex, Image, Text} from '@chakra-ui/react'
+import Card from '../components/Card'
+import { logo } from '../images/logo.png'
+import fullBackground from '../images/backgroundImage.png'
+import { GlobalStyle } from '@chakra-ui/react'
+import { GlobalStyles } from '../constants/GlobalStyles'
 // import 'firebase/database'
 import firebaseConfig from '../repository/firebase/firebase'
 import firebase from 'firebase/app';
 import { useNavigate } from 'react-router-dom';
 import SideDrawer from "../components/Drawer";
+import Navbar from "../components/Navbar";
+import chef from '../images/chef.png'
+import burger from "../images/contactBurger.png";
 
 
 
@@ -58,13 +66,58 @@ const About = (item) => {
 
 
     return (
-        <div>
-            <SideDrawer/>
-            {/*<button onClick={() => addToCart(data)}></button>*/}
-            <Box>
-                About
-            </Box>
-        </div>
+            // <SideDrawer/>
+            // <button onClick={() => addToCart(data)}></button>
+        //...
+
+        <Box
+            minHeight={'100vh'}
+            bgImage={fullBackground}
+            bgPosition={'center'}
+            bgRepeat={'no-repeat'}
+            bgSize={'cover'}
+        >
+            <Navbar />
+            <Flex
+                flexDir={["column-reverse","column-reverse","row"]}
+                maxH={'90vh'}
+            >
+                <Box
+                    
+                    width={['100%', '100%', '50%']}
+                    height={['45vh', '45vh', 'auto']}
+                    display={'flex'}
+                    justifyContent={'center'}
+                    alignItems={'center'}
+                >
+                    <Image
+                        src={chef}
+                        alt={'Chef'}
+                        objectFit={"contain"}
+                        height={['auto', 'auto', '100%']}
+                        maxHeight={["45vh","45vh","100vh"]}
+
+
+                    />
+                </Box>
+                <Box width={['100%', '100%', '50%']} height={['45vh', '45vh', 'auto']}>
+                    <Box display={'flex'} flexDir={'column'} justifyContent={'center'} p={[4, 6, 8]}>
+                        <Text fontSize={["55px","55px","65px","110px"]} fontFamily={'Norwester'} color={'white'}>
+                            Our
+                        </Text>
+                        <Text mt={[-3, 0, -5]} mb={[2, 0, 2]} fontSize={["60px","50px","60px","100px"]} fontFamily={'Norwester'} color={GlobalStyles.colors.secondary}>
+                            Story
+                        </Text>
+                        <Text color={'white'} fontSize={['14.5px', '16px', '18px', '22px']} mb={4} fontFamily={"Montserrat"}>
+                            Digit Bite: Where every tap bites into a world of flavors! Explore diverse dishes in our wide range menu. Whether you’re craving comfort or culinary adventures, Digit Bite ensures that great food is just a tap away – making your dining journey convenient, enjoyable, and delicious.
+                        </Text>
+                        <Button color={'white'} width={["200px"]} mt={6} pl={"48px"} pr={"48px"} pb={6} pt={6} mb={"40px"} borderRadius={30} bg={GlobalStyles.colors.secondary} fontSize={'25px'} fontFamily={"Studly"}>
+                            OUR MENU
+                        </Button>
+                    </Box>
+                </Box>
+            </Flex>
+        </Box>
 
     )
 }
