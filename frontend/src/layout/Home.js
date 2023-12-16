16b9c2cef2c2a362863bf52507350ba357632812
AceGjorgjievski <ace.gjorgjievski6@gmail.com>
2023-12-16 12:45:40 +0100

    modified the README.md file and also the web app is online! :)

--- frontend/src/layout/Home.js
+++ frontend/src/layout/Home.js
@@ -12,78 +12,30 @@ import { useNavigate } from 'react-router-dom';
 import burger from '../images/burger.png'
 import Drawer from "../components/Drawer";
 import {Link} from "react-router-dom";
-const Home = () => {
 
+const Home = () => {
     const [user, setUser] = useState(null);
     const navigate = useNavigate();
-    firebase.auth().onAuthStateChanged((user) => {
-        if (user) {
-            setUser(user);
-            console.log('This is the user: ', user.uid)
-        } else {
-            // No user is signed in.
-            setUser(null);
-            console.log('There is no logged in user');
-        }
-    });
+
+    useEffect(() => {
+        const unsubscribe = firebase.auth().onAuthStateChanged((authUser) => {
+            if (authUser) {
+                setUser(authUser);
+                console.log('This is the user:', authUser.uid);
+            } else {
+                setUser(null);
+                console.log('There is no logged-in user');
+            }
+        });
+
+        // Cleanup the subscription when the component unmounts
+        return () => unsubscribe();
+    }, []);
 
     const handleTryItClick = () => {
         // Navigate to the '/menu' route
         navigate('/menu');
     };
-
-    // console.log("final user", {user});
-
-
-    // function GetCurrentUser() {
-    //     const [user, setUser] = useState(null);
-
-    //     useEffect(() => {
-    //         const fetchData = async () => {
-    //             try {
-    //                 const currentUser = await new Promise((resolve, reject) => {
-    //                     const unsubscribe = auth.onAuthStateChanged((user) => {
-    //                         if (user) {
-    //                             resolve(user);
-    //                         } else {
-    //                             resolve(null);
-    //                         }
-    //                         // Cleanup the subscription when the component unmounts
-    //                         return () => unsubscribe();
-    //                     });
-    //                 });
-    //
-    //                 if (currentUser) {
-    //                     // User is signed in.
-    //                     setUser(currentUser);
-    //                     // You can perform additional actions with the user object if needed.
-    //                     // For example, fetch additional user data from Firestore.
-    //                     const userRef = db.collection('users').doc(currentUser.uid);
-    //                     console.log("userRef",userRef.firestore.df)
-    //                     const doc = await userRef.get();
-    //                     if (doc.exists) {
-    //                         console.log('Document data:', doc.data());
-    //                     } else {
-    //                         console.log('No such document!');
-    //                     }
-    //                 } else {
-    //                     // No user is signed in.
-    //                     setUser(null);
-    //                 }
-    //             } catch (error) {
-    //                 console.error('Error fetching user:', error);
-    //             }
-    //         };
-    //
-    //         fetchData();
-    //     }, []);
-    //
-    //     return user?.email || null;
-    // }
-
-    // const user = GetCurrentUser();
-    // console.log("final user ",user);
-
 
     return (
         <Box
@@ -94,15 +46,10 @@ const Home = () => {
             bgSize={'cover'}
         >
             <Navbar />
-            <Box ml={[2, 4, 300]} mt={[10, 15, 110]} display={"flex"} flexDirection={["column", "column", "row-reverse"]} alignItems={"center"}>
+            <Box ml={[2, 4, 300]} mt={[10, 15, 110]} display={'flex'} flexDirection={['column', 'column', 'row-reverse']} alignItems={'center'}>
                 <Box>
                     <Flex flexDirection={['column', 'column', 'column']}>
-                        <Box
-                            color="white"
-                            fontSize={[50, 70, 80, 100]}
-                            fontFamily={'Norwester'}
-                            mb={[-6, -8, -10]}
-                        >
+                        <Box color="white" fontSize={[50, 70, 80, 100]} fontFamily={'Norwester'} mb={[-6, -8, -10]}>
                             DIGIT BITE
                         </Box>
                         <Box
@@ -125,42 +72,42 @@ const Home = () => {
                     >
                         Digit Bite Burger: Elevate Your Culinary Experience with Lightning-Fast
                         Fast Food Indulgences – Unveiling an Array of Quick Bites for On-the-Go
-                        Gratification and Instant Flavorful Bliss!"
+                        Gratification and Instant Flavorful Bliss!
                     </Text>
                     <Flex>
-<<<<<<< HEAD
-                        <Button
-                            color={'white'}
-                            mt={6}
-                            pl={'48px'}
-                            pr={'48px'}
-                            pb={6}
-                            pt={6}
-                            mb={'40px'}
-                            borderRadius={30}
-                            bg={GlobalStyles.colors.secondary}
-                            fontSize={'25px'}
-                            fontFamily={'Studly'}
-                            onClick={handleTryItClick}
-                        >
-=======
-                        <Link to={"/menu"}>
-                        <Button color={'white'} mt={6} pl={"48px"} pr={"48px"} pb={6} pt={6} mb={"40px"} borderRadius={30} bg={GlobalStyles.colors.secondary} fontSize={'25px'} fontFamily={"Studly"}>
->>>>>>> not-found-page
-                            TRY IT
-                        </Button>
+                        <Link to="/menu">
+                            <Button
+                                color={'white'}
+                                mt={6}
+                                pl={'48px'}
+                                pr={'48px'}
+                                pb={6}
+                                pt={6}
+                                mb={'40px'}
+                                borderRadius={30}
+                                bg={GlobalStyles.colors.secondary}
+                                fontSize={'25px'}
+                                fontFamily={'Studly'}
+                                onClick={handleTryItClick}
+                            >
+                                TRY IT
+                            </Button>
                         </Link>
                         <Drawer />
                     </Flex>
                 </Box>
 
-                <Box flexShrink={0} flexWrap={"wrap"}  mb={[0, 0, 0]} mr={[0,0, 0, "90px"]}  maxHeight={["auto","auto","60vh"]} width={["100%", "100%", 500, 650]}>
+                <Box
+                    flexShrink={0}
+                    flexWrap={'wrap'}
+                    mb={[0, 0, 0]}
+                    mr={[0, 0, 0, '90px']}
+                    maxHeight={['auto', 'auto', '60vh']}
+                    width={['100%', '100%', 500, 650]}
+                >
                     <img
                         src={burger}
                         alt="Burger"
-                        // Adjust based on your design
-
-
                         style={{
                             flexShrink: 1,
                             transform: 'translateY(-50px)', // Default translateY value
@@ -175,12 +122,9 @@ const Home = () => {
                         }}
                     />
                 </Box>
-
             </Box>
         </Box>
     );
 };
 
-
-
-export default Home
+export default Home;
